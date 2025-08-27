4f43dbe60f582cf5f6ca7b51799f19dfebca0f3b
Cassio-hue <cassio.borges@singular.tec.br>
2025-08-27 08:46:30 -0300

    tarefa 9: refatorando scan das páginas anotadas com @Rota

--- src/main/java/servlet/MiniServletMVC.java
+++ src/main/java/servlet/MiniServletMVC.java
@@ -16,17 +16,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MiniServletMVC extends HttpServlet {
+    private static final Logger LOGGER = Logger.getLogger(MiniServletMVC.class.getName());
+    private final Map<String, Class<?>> UriPageMap = new HashMap<>();
+
+
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String uri = request.getRequestURI();
-        Map<String, Object> parameters = new HashMap<>();
-        request.getParameterMap().forEach((key, value) -> {
-            parameters.put(key, value[0]);
-        });
-
-        response.setContentType("text/html; charset=UTF-8");
-        Page page = null;
-
+    public void init() {
         String pkg = "servlet.pages";
         String routeAnnotation = Rota.class.getName();
         try (ScanResult scanResult =
@@ -38,16 +33,31 @@ public class MiniServletMVC extends HttpServlet {
                 AnnotationInfo routeAnnotationInfo = routeClassInfo.getAnnotationInfo(routeAnnotation);
                 List<AnnotationParameterValue> routeParamVals = routeAnnotationInfo.getParameterValues();
                 String route = (String) routeParamVals.getFirst().getValue();
-                if (uri.equals(route)) {
-                    Class<?> clazz = routeClassInfo.loadClass();
-                    try {
-                        Object instance = clazz.getDeclaredConstructor().newInstance();
-                        page = (Page) instance;
-                        break;
-                    } catch (Exception e) {
-                        Logger.getLogger(MiniServletMVC.class.getName()).log(Level.SEVERE, "Erro ao construir página: " + routeClassInfo.getName(), e);
-                    }
-                }
+
+                Class<?> clazz = routeClassInfo.loadClass();
+                UriPageMap.put(route, clazz);
+            }
+        }
+    }
+
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String uri = request.getRequestURI();
+        Map<String, Object> parameters = new HashMap<>();
+        request.getParameterMap().forEach((key, value) -> {
+            parameters.put(key, value[0]);
+        });
+        response.setContentType("text/html; charset=UTF-8");
+
+        Page page = null;
+        if (UriPageMap.containsKey(uri)) {
+            Class<?> clazz = UriPageMap.get(uri);
+            try {
+                Object instance = clazz.getDeclaredConstructor().newInstance();
+                page = (Page) instance;
+            } catch (Exception e) {
+                LOGGER.log(Level.SEVERE, "Erro ao instanciar página: " + clazz.getName(), e);
             }
         }
 
