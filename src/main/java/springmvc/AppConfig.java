82fb07bdf3a1201d7511817f6d282f524833c763
Cassio-hue <cassio.borges@singular.tec.br>
2025-09-02 16:51:14 -0300

    tarefa 13: usando tela de login do spring

--- src/main/java/springmvc/AppConfig.java
+++ src/main/java/springmvc/AppConfig.java
@@ -7,6 +7,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -81,10 +82,7 @@ public class AppConfig {
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers(custom.matcher("/*")).authenticated()
                 .anyRequest().authenticated())
-        .formLogin(form -> form
-                .loginPage("/login")
-                .permitAll()
-        );
+        .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
